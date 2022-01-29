980ce2de4e12eb4f5bac60bfce266d9b32f86829
Graeme Winter <graeme.winter@gmail.com>
2022-01-29 13:08:32 +0000

    Cleanup 1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageDraw, ImageFont
 
 games = pickle.load(open("games_short.p", "rb"))
 mx = len(games)
-index = random.randrange(0,mx)
+index = random.randrange(0, mx)
 
 grid = []
 counter = 0
@@ -14,7 +14,7 @@ for i in range(9):
     row = []
     for j in range(9):
         row.append(int(games[index][counter]))
-        counter+=1
+        counter += 1
     grid.append(row)
 
 
@@ -23,16 +23,16 @@ row_sums = []
 for row in grid:
     one = row.index(1)
     nine = row.index(9)
-    if abs(one-nine) == 1:
+    if abs(one - nine) == 1:
         row_sums.append(0)
     elif one > nine:
         sm = 0
-        for i in range(nine+1, one):
+        for i in range(nine + 1, one):
             sm += row[i]
         row_sums.append(sm)
     else:
         sm = 0
-        for i in range(one+1, nine):
+        for i in range(one + 1, nine):
             sm += row[i]
         row_sums.append(sm)
 
@@ -49,41 +49,27 @@ col_sums = []
 for col in cols:
     one = col.index(1)
     nine = col.index(9)
-    if abs(one-nine) == 1:
+    if abs(one - nine) == 1:
         col_sums.append(0)
     elif one > nine:
         sm = 0
-        for i in range(nine+1, one):
+        for i in range(nine + 1, one):
             sm += col[i]
         col_sums.append(sm)
     else:
         sm = 0
-        for i in range(one+1, nine):
+        for i in range(one + 1, nine):
             sm += col[i]
         col_sums.append(sm)
 
-
-        
-
-for row in grid:
-    print(row)
-
-print('\n')
-print(col_sums)
-print(row_sums)
-
-
-
-
-sol = Image.new('RGB', (1200,1200), color='white')
+sol = Image.new("RGB", (1200, 1200), color="white")
 s_draw = ImageDraw.Draw(sol)
 
-puz = Image.new('RGB', (1200, 1200), color='white')
+puz = Image.new("RGB", (1200, 1200), color="white")
 p_draw = ImageDraw.Draw(puz)
 
-s_draw.rectangle([(0, 0), (1200, 1200)], outline='black', width=10)
-fnt = ImageFont.truetype('arial.ttf', 60)
-
+s_draw.rectangle([(0, 0), (1200, 1200)], outline="black", width=10)
+fnt = ImageFont.truetype("Arial Unicode.ttf", 60)
 
 # generate starting squares
 xy = []
@@ -94,41 +80,87 @@ while len(xy) < 10:
 
 # draw 9 x 9 grid with numbers
 
-for i in range(1,10):
-    for j in range(1,10):
-        s_draw.rectangle([(i*100, j*100), ((i+1)*100, (j+1)*100)], outline='black')
-        p_draw.rectangle([(i*100, j*100), ((i+1)*100, (j+1)*100)], outline='black')
-        s_draw.text((i*100+35, j*100+15), '{}'.format(grid[i-1][j-1]), font=fnt, fill=(0,0,0,255))
+for i in range(1, 10):
+    for j in range(1, 10):
+        s_draw.rectangle(
+            [(i * 100, j * 100), ((i + 1) * 100, (j + 1) * 100)], outline="black"
+        )
+        p_draw.rectangle(
+            [(i * 100, j * 100), ((i + 1) * 100, (j + 1) * 100)], outline="black"
+        )
+        s_draw.text(
+            (i * 100 + 35, j * 100 + 15),
+            "{}".format(grid[i - 1][j - 1]),
+            font=fnt,
+            fill=(0, 0, 0, 255),
+        )
 
-        if (i-1, j-1) in xy:
-            p_draw.text((i*100+35, j*100+15), '{}'.format(grid[i-1][j-1]), font=fnt, fill=(0,0,0,255))
-    
+        if (i - 1, j - 1) in xy:
+            p_draw.text(
+                (i * 100 + 35, j * 100 + 15),
+                "{}".format(grid[i - 1][j - 1]),
+                font=fnt,
+                fill=(0, 0, 0, 255),
+            )
 
 
 # draw 3 x 3 thick grid
 for i in range(3):
     for j in range(3):
-        s_draw.rectangle([(i*300+100, j*300+100), ((i+1)*300+100, (j+1)*300+100)], outline='black', width=5)
-        p_draw.rectangle([(i*300+100, j*300+100), ((i+1)*300+100, (j+1)*300+100)], outline='black', width=5)
+        s_draw.rectangle(
+            [
+                (i * 300 + 100, j * 300 + 100),
+                ((i + 1) * 300 + 100, (j + 1) * 300 + 100),
+            ],
+            outline="black",
+            width=5,
+        )
+        p_draw.rectangle(
+            [
+                (i * 300 + 100, j * 300 + 100),
+                ((i + 1) * 300 + 100, (j + 1) * 300 + 100),
+            ],
+            outline="black",
+            width=5,
+        )
 
 
 # draw row + column sums
 for i in range(9):
-    s_draw.text((1025, i*100+110), '{}'.format(col_sums[i]), font=fnt, fill=(0,0,0,255))
-    p_draw.text((1025, i*100+110), '{}'.format(col_sums[i]), font=fnt, fill=(0,0,0,255))
-    if int(row_sums[i]/10)==0:
-        s_draw.text((i*100+135, 1015), '{}'.format(row_sums[i]), font=fnt, fill=(0,0,0,255))
-        p_draw.text((i*100+135, 1015), '{}'.format(row_sums[i]), font=fnt, fill=(0,0,0,255))
+    s_draw.text(
+        (1025, i * 100 + 110), "{}".format(col_sums[i]), font=fnt, fill=(0, 0, 0, 255)
+    )
+    p_draw.text(
+        (1025, i * 100 + 110), "{}".format(col_sums[i]), font=fnt, fill=(0, 0, 0, 255)
+    )
+    if int(row_sums[i] / 10) == 0:
+        s_draw.text(
+            (i * 100 + 135, 1015),
+            "{}".format(row_sums[i]),
+            font=fnt,
+            fill=(0, 0, 0, 255),
+        )
+        p_draw.text(
+            (i * 100 + 135, 1015),
+            "{}".format(row_sums[i]),
+            font=fnt,
+            fill=(0, 0, 0, 255),
+        )
     else:
-        s_draw.text((i*100+120, 1015), '{}'.format(row_sums[i]), font=fnt, fill=(0,0,0,255))
-        p_draw.text((i*100+120, 1015), '{}'.format(row_sums[i]), font=fnt, fill=(0,0,0,255))
+        s_draw.text(
+            (i * 100 + 120, 1015),
+            "{}".format(row_sums[i]),
+            font=fnt,
+            fill=(0, 0, 0, 255),
+        )
+        p_draw.text(
+            (i * 100 + 120, 1015),
+            "{}".format(row_sums[i]),
+            font=fnt,
+            fill=(0, 0, 0, 255),
+        )
 
 
-
-
-
-
-sol.save('solution.png', 'PNG')
+sol.save("solution.png", "PNG")
 puz.show()
-puz.save('puzzle.png', 'PNG')
-
+puz.save("puzzle.png", "PNG")
